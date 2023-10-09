51180b9bd3eea74da1dda77d62c3fcba6d235922
v1vaGIT <v1vatoxic@gmail.com>
2023-10-10 08:03:01 +1000

    убрал логи

--- src/store/store.ts
+++ src/store/store.ts
@@ -64,7 +64,6 @@ export default class Store {
   async login(email: string, password: string) {
     try {
       const response = await AuthService.login(email, password);
-      console.log(response);
       localStorage.setItem("token", response.data.token);
       this.setAuth(true);
       this.setViewer(response.data.viewer);
@@ -94,7 +93,6 @@ export default class Store {
           withCredentials: true,
         },
       );
-      console.log("response", response);
       localStorage.setItem("token", response.data.token);
       this.setAuth(true);
       this.setViewer(response.data.viewer);
@@ -170,7 +168,6 @@ export default class Store {
     this.setLoading(true);
     try {
       const response = await CoursesService.signUpOnCourse(id);
-      console.log(response);
       alert("Вы успешно записались на курс.");
     } catch (e: any) {
       console.log(e.response?.data?.message);
