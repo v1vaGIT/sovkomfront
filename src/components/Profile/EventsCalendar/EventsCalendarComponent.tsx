4125665593cd381c13fac8e5fac900d8f37570c0
Двоичный кот <skorikovdanila@mail.ru>
2023-10-10 11:48:01 +1000

    Стилизация календаря

--- src/components/Profile/EventsCalendar/EventsCalendarComponent.tsx
+++ src/components/Profile/EventsCalendar/EventsCalendarComponent.tsx
@@ -3,25 +3,28 @@ import React, { FC } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 
+import './style.css'
+
 import { events } from '../../../../fakeData/events.js'
 import { observer } from "mobx-react-lite";
 const EventsCalendarComponent: FC = () =>  {
   let firstDaty = 1;
 
+  
+
   return (
     <>
       <FullCalendar
-        //defaultView="dayGridMonth"
         firstDay={firstDaty}
         locale="ru"
-        // header={{
-        //   left: "prev,next",
-        //   center: "title",
-        //   right: "dayGridMonth,timeGridWeek,timeGridDay"
-        // }}
-        themeSystem="Simplex"
+        headerToolbar={{
+          left: "prev,next",
+          center: "title",
+          right: "today"
+        }}
+        themeSystem="Cosmo"
         plugins={[dayGridPlugin]}
-        events={events.data}
+        events={events}
       />
     </>
   );
